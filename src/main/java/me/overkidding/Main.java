c183f7476c126d6ba022d6d13f863577a33773c7
overkidding <46853321+overkidding@users.noreply.github.com>
2023-06-01 23:09:06 +0200

    Now using thread pool

--- src/main/java/me/overkidding/Main.java
+++ src/main/java/me/overkidding/Main.java
@@ -5,8 +5,9 @@ import at.yawk.votifier.VotifierServerBuilder;
 import me.overkidding.votifier.VotifierClient;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -14,7 +15,7 @@ public class Main {
     public static File CONFIG = new File("config.json");
     public static VotifierClient client;
 
-    private static final List<Thread> threads = new ArrayList<>();
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(2);
 
     public static void main(String[] args) {
         try {
@@ -35,28 +36,36 @@ public class Main {
                     .key(VotifierKeyPair.read(KEY))
                     .voteListener(vote -> {
                         System.out.println("Received vote from " + vote.getUsername() + " (" + vote.getAddress() + ") by " + vote.getService());
-                        Thread t = new Thread(() -> {
+                        executorService.submit(() -> {
                             try {
                                 client.send(vote);
-                            }catch (Exception ex){
-                                ex.printStackTrace();
+                            } catch (Exception e) {
+                                e.printStackTrace();
                             }
                         });
-                        threads.add(t);
-                        t.start();
                     }).start();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            for(Thread t : threads){
-                try {
-                    t.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            try {
+                shutdownAndAwaitTermination();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }));
+    }
+
+    private static final int n = 60;
+
+    private static void shutdownAndAwaitTermination() throws InterruptedException {
+        executorService.shutdown();
+        if (!executorService.awaitTermination(n, TimeUnit.SECONDS)) {
+            executorService.shutdownNow();
+            if (!executorService.awaitTermination(n, TimeUnit.SECONDS)) {
+                System.out.println("The pool did not terminate");
+            }
+        }
     }
 }
