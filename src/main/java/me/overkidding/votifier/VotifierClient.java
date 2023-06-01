a1c1c1117ef4d80a81ac1ee3e4def7b560a51d8d
overkidding <46853321+overkidding@users.noreply.github.com>
2023-06-01 23:46:42 +0200

    More try with resources

--- src/main/java/me/overkidding/votifier/VotifierClient.java
+++ src/main/java/me/overkidding/votifier/VotifierClient.java
@@ -13,7 +13,8 @@ public class VotifierClient extends VotifierImplementation {
 
     private final String address;
     private final int port;
-    @Setter private String token;
+    @Setter
+    private String token;
 
     public VotifierClient(String address, int port, String token) {
         this.address = address;
@@ -22,24 +23,21 @@ public class VotifierClient extends VotifierImplementation {
     }
 
     public void send(Vote vote) {
-        try(Socket socket = new Socket()){
+        try (Socket socket = new Socket()) {
             socket.connect(new InetSocketAddress(address, port), 10000);
 
-            InputStream is = socket.getInputStream();
-            OutputStream os = socket.getOutputStream();
+            try (InputStream is = socket.getInputStream() ; OutputStream os = socket.getOutputStream() ; BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+                String in = br.readLine();
+                String[] splitIn = in.split(" ");
+                String challengeToken = splitIn[2];
 
-            String in = new BufferedReader(new InputStreamReader(is)).readLine();
-            String[] splitIn = in.split(" ");
-            String challengeToken = splitIn[2];
-
-            byte[] message = encode(vote, challengeToken, token);
-            os.write(message);
-            os.flush();
-            System.out.println(new BufferedReader(new InputStreamReader(is)).readLine());
-
-            os.close();
-            System.out.println("Sent vote to " + address + ":" + port + " with token " + challengeToken);
-        }catch (Exception e){
+                byte[] message = encode(vote, challengeToken, token);
+                os.write(message);
+                os.flush();
+                System.out.println(br.readLine());
+                System.out.println("Sent vote to " + address + ":" + port + " with token " + challengeToken);
+            }
+        } catch (Exception e) {
             System.out.println("Error while sending vote: " + e.getMessage());
         }
     }
